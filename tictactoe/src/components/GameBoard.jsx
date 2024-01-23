e8ee4e63ec4bd86dbead0be1f1a64528fb16c080
Nikita Migushev <migushev@gmail.com>
2024-01-23 22:10:51 +0300

    added reactInvestmentCalculator project

--- tictactoe/src/components/GameBoard.jsx
+++ tictactoe/src/components/GameBoard.jsx
@@ -1,77 +1,22 @@
-
-
-/*
-First we start from creating the initial board
-*/
-
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-];
-
-export default function GameBoard({onSelectSquare, turns}) {
-    let gameBoard = initialGameBoard;
-
-    for (const turn of turns) {
-        const {square, player} = turn;
-        const {row, col} = square;
-
-        gameBoard[row][col] = player;
-    }
-
-    // //Here we create a state of board
-    //
-    // const [gameBoard, setGameBoard] = useState(initialGameBoard); //initialGameBard is set as default value of state
-    //
-    //
-    // /*
-    // number, number -> Board
-    // takes rowIndex and colIndex as input, defines cell, and returns an updated Board with filled
-    // 'X' symbol on defined cell.
-    //  */
-    // function handleSelectSquare(rowIndex, colIndex) {
-    //     // take previous gameboard in setFunction
-    //     setGameBoard((prevGameBoard) => {
-    //         /*
-    //         create a copy of previous board and call it updated board
-    //         */
-    //         const updatedBoard =
-    //             /*
-    //             Array -> Array
-    //             take previous board array and destructure it into array of rows, make a copy
-    //             */
-    //             [...prevGameBoard
-    //
-    //                 /*
-    //                 Array -> Array
-    //                 takes array of rows and apply function to each element to destructure its innerArray, make a copy
-    //                 */
-    //                 .map(innerArray => [...innerArray])];
-    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-    //         return updatedBoard;
-    //     });
-    //
-    //     onSelectSquare();
-    // }
-/*
-Gameboard -> Gameboard JSX render
-take gameboard and renders rows and columns, in selected cell calls function handleSelectedSquare where
-board is updated
-*/
-    return (
-        <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => (
-                <li key={rowIndex}>
-                    <ol>
-                        {row.map((playerSymbol, colIndex) => (
-                            <li key={colIndex}>
-                                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
-                            </li>
-                        ))}
-                    </ol>
-                </li>
+export default function GameBoard({ onSelectSquare, board }) {
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
             ))}
-        </ol>
-    );
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
 }
--- tictactoe/src/components/Player.jsx
+++ tictactoe/src/components/Player.jsx
@@ -1,33 +1,43 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
-export default function Player({initialName, symbol, isActive}) {
-    const [playerName, setPlayerName] = useState(initialName)
-    const [isEditing, setIsEditing] = useState(false);
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
 
-    let buttonCaption = 'Edit';
-
-    function handleEditClick() {
-        setIsEditing((editing) => !editing);
-    }
-
-    function handleChange() {
-        setPlayerName(event.target.value)
-    }
-
-    let editablePlayerName = <span className="player-name">{playerName}</span>;
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
 
     if (isEditing) {
-        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>;
-        buttonCaption = 'Save'
+      onChangeName(symbol, playerName);
     }
+  }
 
-    return (
-        <li className={isActive ? 'active' : undefined}>
-           <span className="player">
-               {editablePlayerName}
-               <span className="player-symbol">{symbol}</span>
-             </span>
-            <button onClick={handleEditClick}>{buttonCaption}</button>
-        </li>
-    )
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  // let btnCaption = 'Edit';
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+    // btnCaption = 'Save';
+  }
+
+  return (
+    <li className={isActive ? 'active' : undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+    </li>
+  );
 }
