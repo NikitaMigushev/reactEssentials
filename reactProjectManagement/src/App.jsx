5b23a25ca198b1eeef4f624d6b85d490ac6e4811
Nikita Migushev <migushev@gmail.com>
2024-01-30 20:34:51 +0300

    project management app

--- reactProjectManagement/src/App.jsx
+++ reactProjectManagement/src/App.jsx
@@ -1,13 +1,59 @@
-import ProjectSideBar from "./components/ProjectsSidebar.jsx";
-import NewProject from "./components/NewProject.jsx";
+import { useState } from 'react';
+
+import NewProject from './components/NewProject.jsx';
+import NoProjectSelected from './components/NoProjectSelected.jsx';
+import ProjectsSidebar from './components/ProjectsSidebar.jsx';
 
 function App() {
-  return (
-    <main className="h-screen my-8 flex gap-8">
-      <ProjectSideBar/>
-        <NewProject />
-    </main>
-  );
+    const [projectsState, setProjectsState] = useState({
+        selectedProjectId: undefined,
+        projects: [],
+    });
+
+    function handleStartAddProject() {
+        setProjectsState((prevState) => {
+            return {
+                ...prevState,
+                selectedProjectId: null,
+            };
+        });
+    }
+
+    function handleAddProject(projectData) {
+        setProjectsState((prevState) => {
+            const projectId = Math.random();
+            const newProject = {
+                ...projectData,
+                id: projectId,
+            };
+
+            return {
+                ...prevState,
+                selectedProjectId: undefined,
+                projects: [...prevState.projects, newProject],
+            };
+        });
+    }
+
+    let content;
+
+    console.log(projectsState)
+
+    if (projectsState.selectedProjectId === null) {
+        content = <NewProject onAdd={handleAddProject} />;
+    } else if (projectsState.selectedProjectId === undefined) {
+        content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+    }
+
+    return (
+        <main className="h-screen my-8 flex gap-8">
+            <ProjectsSidebar
+                onStartAddProject={handleStartAddProject}
+                projects={projectsState.projects}
+            />
+            {content}
+        </main>
+    );
 }
 
 export default App;
