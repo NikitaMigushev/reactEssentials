e494d4d0e469dbe227abb5cb46523c69ad0d8f8e
Nikita Migushev <migushev@gmail.com>
2024-02-09 15:17:28 +0300

    react routing

--- reactRouting/src/App.js
+++ reactRouting/src/App.js
@@ -1,5 +1,35 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetailPage";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <RootLayout />,
+        errorElement: <ErrorPage/>,
+        children: [
+            {
+                path: '/',
+                element: <HomePage/>
+            },
+            {
+                path: '/products',
+                element: <ProductsPage/>
+            },
+            {
+                path: '/products/:productId', element: <ProductDetailPage />
+            }
+
+        ]
+    },
+
+]);
+
 function App() {
-  return <div></div>;
+    return <RouterProvider router={router}/>;
 }
 
 export default App;
