cbebf7c295bee960d197da13926193b44039a986
Nikita Migushev <migushev@gmail.com>
2024-02-12 16:32:04 +0300

    react query

--- reactQuery/src/components/Events/EditEvent.jsx
+++ reactQuery/src/components/Events/EditEvent.jsx
@@ -1,27 +1,89 @@
-import { Link, useNavigate } from 'react-router-dom';
+import {Link, useNavigate, useParams} from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import {focusManager, useMutation, useQuery} from "@tanstack/react-query";
+import {fetchEvent, queryClient, updateEvent} from "../../util/http.js";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EditEvent() {
-  const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+    const navigate = useNavigate();
+    const params = useParams();
 
-  function handleClose() {
-    navigate('../');
-  }
+    const {data, isPending, isError, error} = useQuery({
+        queryKey: ['events', params.id],
+        queryFn: ({signal}) => fetchEvent({signal, id: params.id})
+    });
 
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
-      </EventForm>
-    </Modal>
-  );
+    const {mutate} = useMutation({
+        mutationFn: updateEvent,
+        onMutate: async (data) => {
+            const newEvent = data.event;
+            await queryClient.cancelQueries({queryKey: ['events', params.id]})
+            const previousEvent = queryClient.getQueryData(['events', params.id])
+            queryClient.setQueryData(['events', params.id], newEvent);
+
+            return { previousEvent}
+        },
+        onError: (error, data, context) => {
+            queryClient.setQueryData(['events', params.id], context.previousEvent)
+        },
+        onSettled: () => {
+            queryClient.invalidateQueries(['events', params.id])
+        }
+    });
+
+
+    function handleSubmit(formData) {
+        mutate({id: params.id, event: formData});
+        navigate('../');
+    }
+
+    function handleClose() {
+        navigate('../');
+    }
+
+    let content;
+
+    if (isPending) {
+        content = (<div className="center">
+                <LoadingIndicator/>
+            </div>
+        );
+    }
+
+    if (isError) {
+        content = (
+            <>
+                <ErrorBlock title="Failed to load event"
+                            message={error.info?.message || 'Failed to load event. Please check your inputs and try again later.'}>
+                    <div className="form-actions"></div>
+                    <Link to="../" className="button">
+                        Okay
+                    </Link>
+                </ErrorBlock>
+            </>
+        )
+    }
+
+    if (data) {
+        content = (
+            <EventForm inputData={data} onSubmit={handleSubmit}>
+                <Link to="../" className="button-text">
+                    Cancel
+                </Link>
+                <button type="submit" className="button">
+                    Update
+                </button>
+            </EventForm>
+        )
+    }
+
+    return (
+        <Modal onClose={handleClose}>
+            {content}
+        </Modal>
+    );
 }
