71701e3efccdc539f9c4ee4453f8b95e852eb294
Nikita Migushev <migushev@gmail.com>
2024-02-13 14:33:54 +0300

    react patterns

--- reactPatterns/src/App.jsx
+++ reactPatterns/src/App.jsx
@@ -1,5 +1,22 @@
 import Accordion from './components/Accordion/Accordion.jsx';
 import AccordionTitle from "./components/Accordion/AccordionTitle.jsx";
+import SearchableList from "./components/SearchableList/SeatchableList.jsx";
+
+const places = [
+    {
+        name: 'Place1',
+        description: 'Description place 1'
+    },
+    {
+        name: 'Place2',
+        description: 'Description place 2'
+    },
+    {
+        name: 'Place3',
+        description: 'Description place 3'
+    },
+]
+
 
 function App() {
     return (
@@ -43,6 +60,14 @@ function App() {
                         </Accordion.Content>
                     </Accordion.Item>
                 </Accordion>
+            </section>
+            <section>
+                <SearchableList items={places} itemKeyFn={(item) => item.name}>
+                    {(item) => item.name}
+                </SearchableList>
+                <SearchableList items={['item1', 'item2']} itemKeyFn={(item) => item}>
+                    {(item) => item}
+                </SearchableList>
             </section>
         </main>
     );
